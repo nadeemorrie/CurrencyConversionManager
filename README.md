e21eb88630f0b6d945b16bf9dbd91800530d54a8
Nadeem Orrie <nadeem.orrie@gmail.com>
2017-02-23 11:58:11 +0200

    updated readme, added db scripts

--- README.md
+++ README.md
@@ -1,21 +1,91 @@
-# Currency Conversion Manager
+# Currency Converter Manager
 
-### PHP Framework: Laravel 5.4 
+## Framework Used: Laravel (v5.4)
 
-### Server Requirements
+### Setup a local virtual dev machine
 
-The Laravel framework has a few system requirements. Of course, all of these requirements are satisfied by the Laravel Homestead virtual machine, so it's highly recommended that you use Homestead as your local Laravel development environment.
+###Install Oracle Virtual Box (v5.1)
+Download and install the DEB or DMG file: (https://www.virtualbox.org/wiki/Linux_Downloads).
 
-However, if you are not using Homestead, you will need to make sure your server meets the following requirements:
+##Install Vagrant (v1.9.1)
+Download and install the DEB or DMG file: (https://www.vagrantup.com/downloads.html)
 
-PHP >= 5.6.4
-OpenSSL PHP Extension
-PDO PHP Extension
-Mbstring PHP Extension
-Tokenizer PHP Extension
-XML PHP Extension
+Create a "Code" directory in your home dirctory.
+
+Clone the git repo into your "Code" directory.
+
+Linux/Mac
+$ git clone https://github.com/nadeemorrie/CurrencyConversionManager.git
+
+Open the CurrencyConversionManager folder
+$ cd CurrencyConversionManager/
+
+Laravel utilizes Composer to manage its dependencies. So, before using Laravel, make sure you have Composer installed on your machine.
+
+Run composer update
+$ composer update
+
+The repo/project ships with a Vagrantfile allowing you to run vagrant per project instead of globally.
+
+Install vagrant virtual maching Homestead.
+$ php vendor/bin/homestead make
+
+The default virtual server runs on 192.168.10.10. see Homestead.yaml. you can change this ip to anything you like.
+
+Add the ip in your hosts file. use any editor of your choice.
+$ vi /etc/hosts
+
+** Set it to something like this: 192.168.10.10 dev.currency
+
+Finally, run vagrant from terminal
+$ vagrant up
+
+##Setup the database
+
+## Ssh into the VM.
+$ cd CurrencyConversionManager/
+$ vagrant ssh
+
+Once logged in via ssh
+$ cd currencyconversionmanager
+
+create the database
+$ php artisan migrate
+
+Populate database with data test data for the tables named currencies and actions.
+$ php artisan db:seed
+
+Finally, test the link:
+http://dev.currency
+
+## Mail configuration
+Check the .env file, im using MAILGUN. You need to create a mailgun account and simply fill in the
+Mail_Driver, Mailgun_Domain, Mailgun_Secret.
+If you don't feel like setting up the mail config, that feature will be caught by an exception handler.
 
 
-### Laravel Homestead - (virtual machine)
-If you want to setup a quick server with all of the above installed you can try Laravel Homestead.
-Setup instructions, https://laravel.com/docs/5.4/homestead
+
+# Setup using your own custom server.
+
+You will need to make sure your server meets the following requirements:
+
+- PHP >= 5.6.4
+- OpenSSL PHP Extension
+- PDO PHP Extension
+- Mbstring PHP Extension
+- Tokenizer PHP Extension
+- XML PHP Extension
+
+You need composer.
+
+## MySQL DB
+
+The db script includes creationg of db and tables and population of data.
+
+Browse to CurrencyConversionManager/App/Scripts/DB/currencyManager.sql
+
+### DB Connection Settings
+The DB settings can be configured in the .env file.
+
+
+
---
+++ app/Scripts/DB/currencyManager.sql
@@ -0,0 +1,206 @@
+CREATE DATABASE  IF NOT EXISTS `homestead` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `homestead`;
+-- MySQL dump 10.13  Distrib 5.7.9, for linux-glibc2.5 (x86_64)
+--
+-- Host: 192.168.20.20    Database: homestead
+-- ------------------------------------------------------
+-- Server version	5.7.16-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actions`
+--
+
+DROP TABLE IF EXISTS `actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `currency_id` int(10) unsigned NOT NULL,
+  `type` enum('email','discount') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `discount` double DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `actions_currency_id_foreign` (`currency_id`),
+  CONSTRAINT `actions_currency_id_foreign` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actions`
+--
+
+LOCK TABLES `actions` WRITE;
+/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
+INSERT INTO `actions` VALUES (1,1,NULL,NULL,NULL,NULL,NULL),(2,2,'email',NULL,'nadeem.orrie@gmail.com',NULL,NULL),(3,3,'discount',0.1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `currency`
+--
+
+DROP TABLE IF EXISTS `currency`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `currency` (
+  `id` int(10) unsigned NOT NULL,
+  `baseCode` char(3) COLLATE utf8_unicode_ci NOT NULL,
+  `baseCodeSymbol` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  `code` char(3) COLLATE utf8_unicode_ci NOT NULL,
+  `symbol` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `currency`
+--
+
+LOCK TABLES `currency` WRITE;
+/*!40000 ALTER TABLE `currency` DISABLE KEYS */;
+INSERT INTO `currency` VALUES (1,'USD','$','ZAR','R','South African Rand',NULL,NULL),(2,'USD','$','GBP','£','British Pound',NULL,NULL),(3,'USD','$','EUR','€','Euro',NULL,NULL),(4,'USD','$','KES','K','Kenyan Shilling',NULL,NULL),(5,'RSA','R','USD','$','United Snakes Dollars',NULL,NULL),(6,'RSA','R','GBP','£','British Pound',NULL,NULL),(7,'RSA','R','EUR','€','Euro',NULL,NULL),(8,'RSA','R','KES','K','Kenyan Shilling',NULL,NULL);
+/*!40000 ALTER TABLE `currency` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `customers_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES ('2017_02_01_094950_create_currency_table',1),('2017_02_02_094950_create_rates_table',1),('2017_02_03_094950_create_customer_table',1),('2017_02_04_094950_create_order_table',1),('2017_02_21_075530_create_action_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int(10) unsigned NOT NULL,
+  `rate_id` smallint(5) unsigned NOT NULL,
+  `foreign_amount` double NOT NULL,
+  `cost` double NOT NULL,
+  `surcharge` double NOT NULL,
+  `discount` double DEFAULT NULL,
+  `total` double NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_customer_id_foreign` (`customer_id`),
+  KEY `orders_rate_id_foreign` (`rate_id`),
+  CONSTRAINT `orders_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `orders_rate_id_foreign` FOREIGN KEY (`rate_id`) REFERENCES `rates` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rates`
+--
+
+DROP TABLE IF EXISTS `rates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rates` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `currency_id` int(10) unsigned NOT NULL,
+  `exchange` double NOT NULL,
+  `surcharge` double NOT NULL,
+  `surcharge_percent` double NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rates_currency_id_unique` (`currency_id`),
+  CONSTRAINT `rates_currency_id_foreign` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rates`
+--
+
+LOCK TABLES `rates` WRITE;
+/*!40000 ALTER TABLE `rates` DISABLE KEYS */;
+INSERT INTO `rates` VALUES (1,1,13.0947,0.02,2,NULL,NULL),(2,2,0.805058,0.5,5,NULL,NULL),(3,3,0.938787,0.1,10,NULL,NULL),(4,4,103.538,0.2,2,NULL,NULL),(5,5,10.0947,0.06,6,NULL,NULL),(6,6,2,0.5,5,NULL,NULL),(7,7,3,0.1,10,NULL,NULL),(8,8,50.56,0.03,3,NULL,NULL);
+/*!40000 ALTER TABLE `rates` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-22 10:09:29
